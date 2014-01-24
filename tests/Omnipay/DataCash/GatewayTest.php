26aefe709189ac435da7089d5440f5e5ef8192d7
Andy Coates <andy@websitevision.co.uk>
2014-01-24 16:41:42 +0000

    Corrected test reference

--- tests/Omnipay/DataCash/GatewayTest.php
+++ tests/Omnipay/DataCash/GatewayTest.php
@@ -39,7 +39,7 @@ class GatewayTest extends GatewayTestCase
 
         $this->assertInstanceOf('\Omnipay\DataCash\Message\Response', $response);
         $this->assertTrue($response->isSuccessful());
-        $this->assertEquals('130215141054377801316798', $response->getTransactionReference());
+        $this->assertEquals('123456', $response->getTransactionReference());
     }
 
     public function testPurchaseError()
